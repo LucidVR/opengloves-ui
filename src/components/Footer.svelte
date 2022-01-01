e700d64933a7031195bcc5ca7c90374f2808943c
danwillm <39023874+danwillm@users.noreply.github.com>
2022-01-01 14:43:33 +0000

    update discord link

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,6 +4,6 @@
 
 <footer class="p-4 text-center flex flex-col items-center">
     <p>Source code: <Link url="https://github.com/LucidVR/opengloves-ui">opengloves-ui</Link></p>
-    <p>Discord: <Link url="https://discord.gg/lucidvr">LucidVR Tech Server</Link></p>
+    <p>Discord: <Link url="https://discord.gg/favJqS2k4X">LucidVR Tech Server</Link></p>
     <p>Source code licenced under MIT</p>
 </footer>
