96fe96b191ee642f653e951730db6639de719c34
danwillm <39023874+danwillm@users.noreply.github.com>
2022-01-23 20:53:36 +0000

    change up error messages

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,32 +3,17 @@ import {http} from "@tauri-apps/api";
 const basePath = "http://localhost:18080/";
 
 export const makeHTTPRequest = async (url, method = "POST", body) => {
-    console.log(JSON.stringify(body));
-    // return fetch(basePath + "/" + url, {
-    //     method,
-    //     body: JSON.stringify(body),
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     mode: 'cors'
-    // }).then(response => {
-    //     if(response.ok) return response.text();
-    //
-    //     throw new Error("An unknown error occurred");
-    // });
-
     const response = await http.fetch(basePath + url, {
         method: 'POST',
         body: {
             type: 'Json',
             payload: body,
         },
-        responseType: 1,
+        responseType: 2
     });
-    console.log("hi");
+    if(response.status === 200) return response.data;
 
-    if(response.status === 200) return JSON.stringify(response.data);
-    console.trace(response);
+    console.error(response);
 
-    throw new Error(response.data);
+    throw new Error(response.data ?? "An unknown error occurred");
 }
